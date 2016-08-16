88334919589cd9c666ca85531dade0bda707b939
Jordan Cauley <jordan@cauley.co>
2016-08-16 12:15:02 -0400

    green lights on async

--- app/async.js
+++ app/async.js
@@ -3,9 +3,54 @@ exports = typeof window === 'undefined' ? global : window;
 exports.asyncAnswers = {
   async: function(value) {
 
+    console.log(value);
+
+    var p1 = new Promise(function(resolve, reject) {
+      resolve(value);
+    }).then(function(value){
+      return value;
+    });
+
+    return p1;
+
+
   },
 
   manipulateRemoteData: function(url) {
+
+    var request = new Promise(function(resolve, reject){
+      var client = new XMLHttpRequest();
+      var uri = url;
+
+      client.open('GET', uri);
+      client.send();
+
+      client.onload = function(){
+        if (this.status >= 200 && this.status < 300) {
+          resolve(this.response);
+        } else {
+          reject(this.statusText);
+        }
+      };
+      client.onerror = function () {
+        reject(this.statusText);
+      };
+
+    }).then(function(data){
+
+      var data = JSON.parse(data);
+      var people = data.people;
+      var names = [];
+      people.forEach(function(value, index, array){
+        names.push(value.name);
+      });
+      names.sort();
+      return names;
+      
+    });
+
+    return request;
+
 
   }
 };
